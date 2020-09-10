4b9ee26b21f22db9bf828c0ed3a889cd8751c908
s3815261 <54346194+s3815261@users.noreply.github.com>
2020-09-10 16:18:48 +1000

    Update main.cpp
    Updated the menu, as per a2 spec

--- A2/main.cpp
+++ A2/main.cpp
@@ -4,6 +4,16 @@
 int main(void)
 {
 
-
-
+    std::cout << "Welcome to Azul!" << std::endl;
+    std::cout << "-------------------" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "Menu" << std::endl;
+    std::cout << "1. New Game" << std::endl;
+    std::cout << "2. Load Game" << std::endl;
+    std::cout << "3. Creadits (Show student information)" << std::endl;
+    std::cout << "4. Quit" << std::endl;
+    int option;
+    std::cout << "" << std::endl;
+    std::cout << ">";
+    std::cin >>  option;
 }
