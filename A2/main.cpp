ed1555d3e309897778d8a871dcfb98614abe953d
TommoJohnno <s3781876@student.rmit.edu.au>
2020-09-13 11:40:24 +1000

    Added early functionality for main menu

--- A2/main.cpp
+++ A2/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 
-
 int main(void)
 {
 
@@ -13,7 +12,53 @@ int main(void)
     std::cout << "3. Credits (Show student information)" << std::endl;
     std::cout << "4. Quit" << std::endl;
     int option;
-    std::cout << "" << std::endl;
+    std::cout << std::endl;
     std::cout << ">";
-    std::cin >>  option;
+    std::cin >> option;
+
+    while (option > 4 || option < 1)
+    {
+        std::cout << "Invalid Input" << std::endl;
+        std::cout << ">";
+        std::cin >> option;
+    }
+    if (option == 1)
+    {
+        std::cout << "Starting a New Game" << std::endl;
+        std::cout << std::endl;
+        std::cout << "Enter a name for player 1" << std::endl;
+        std::string player1;
+        std::cout << ">";
+        std::cin >> player1;
+        std::cout << std::endl;
+        std::cout << "Enter a name for player 2" << std::endl;
+        std::string player2;
+        std::cout << ">";
+        std::cin >> player2;
+        std::cout << std::endl;
+        std::cout << "Let's Play!" << std::endl;
+    }
+    else if (option == 2)
+    {
+        std::cout << "Enter the filename from which to load a game" << std::endl;
+        std::cout << ">";
+        std::string filename;
+        std::cin >> filename;
+    }
+    else if (option == 3)
+    {
+        std::cout << "Name: Thomas Johnstone" << std::endl;
+        std::cout << "Student ID: s3781876" << std::endl;
+        std::cout << "Email: s3781876@student.rmit.edu.au" << std::endl;
+        std::cout << "Name: Owen Griffiths" << std::endl;
+        std::cout << "Student ID: " << std::endl;
+        std::cout << "Email: " << std::endl;
+        std::cout << "Name: Jason Tran" << std::endl;
+        std::cout << "Student ID: " << std::endl;
+        std::cout << "Email: " << std::endl;
+    }
+    else if (option == 4)
+    {
+        std::cout << "Goodbye" << std::endl;
+    }
 }
