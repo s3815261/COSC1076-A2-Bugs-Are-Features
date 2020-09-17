164473e1ab2fa6730cc3dba77cbaccc57be920b3
s3840285 <71060205+s3840285@users.noreply.github.com>
2020-09-17 16:32:30 +1000

    Separated the Main Menu function into smaller functions.

--- A2/main.cpp
+++ A2/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
 void mainMenu();
+void newGame();
+void loadGame();
+void credits();
+void quit();
 
 int main(void)
 {
@@ -27,47 +31,67 @@ void mainMenu()
     while (option > 4 || option < 1)
     {
         std::cout << "Invalid Input" << std::endl;
-        std::cout << ">";
+        std::cout << "> ";
         std::cin >> option;
     }
 
     if (option == 1)
     {
-        std::cout << "Starting a New Game" << std::endl;
-        std::cout << std::endl;
-        std::cout << "Enter a name for player 1" << std::endl;
-        std::string player1;
-        std::cout << ">";
-        std::cin >> player1;
-        std::cout << std::endl;
-        std::cout << "Enter a name for player 2" << std::endl;
-        std::string player2;
-        std::cout << ">";
-        std::cin >> player2;
-        std::cout << std::endl;
-        std::cout << "Let's Play!" << std::endl;
+        newGame();
     }
     else if (option == 2)
     {
-        std::cout << "Enter the filename from which to load a game" << std::endl;
-        std::cout << ">";
-        std::string filename;
-        std::cin >> filename;
+        loadGame();
     }
     else if (option == 3)
     {
-        std::cout << "Name: Thomas Johnstone" << std::endl;
-        std::cout << "Student ID: s3781876" << std::endl;
-        std::cout << "Email: s3781876@student.rmit.edu.au" << std::endl;
-        std::cout << "Name: Owen Griffiths" << std::endl;
-        std::cout << "Student ID: " << std::endl;
-        std::cout << "Email: " << std::endl;
-        std::cout << "Name: Jason Tran" << std::endl;
-        std::cout << "Student ID: s3840285" << std::endl;
-        std::cout << "Email: s3840285@student.rmit.edu.au" << std::endl;
+        credits();
     }
     else if (option == 4)
     {
-        std::cout << "Goodbye" << std::endl;
+        quit();
     }
+}
+
+void newGame()
+{
+    std::cout << "Starting a New Game" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Enter a name for player 1" << std::endl;
+    std::string player1;
+    std::cout << "> ";
+    std::cin >> player1;
+    std::cout << std::endl;
+    std::cout << "Enter a name for player 2" << std::endl;
+    std::string player2;
+    std::cout << "> ";
+    std::cin >> player2;
+    std::cout << std::endl;
+    std::cout << "Let's Play!" << std::endl;
+}
+
+void loadGame()
+{
+    std::cout << "Enter the filename from which to load a game" << std::endl;
+    std::cout << "> ";
+    std::string filename;
+    std::cin >> filename;
+}
+
+void credits()
+{
+    std::cout << "Name: Thomas Johnstone" << std::endl;
+    std::cout << "Student ID: s3781876" << std::endl;
+    std::cout << "Email: s3781876@student.rmit.edu.au" << std::endl;
+    std::cout << "Name: Owen Griffiths" << std::endl;
+    std::cout << "Student ID: " << std::endl;
+    std::cout << "Email: " << std::endl;
+    std::cout << "Name: Jason Tran" << std::endl;
+    std::cout << "Student ID: s3840285" << std::endl;
+    std::cout << "Email: s3840285@student.rmit.edu.au" << std::endl;
+}
+
+void quit()
+{
+    std::cout << "Goodbye." << std::endl;
 }
