22f0a1647bbc591243b57f860cb388490ab769d7
TommoJohnno <s3781876@student.rmit.edu.au>
2020-10-04 20:02:43 +1100

    Commenting for Node, Tile and TileBag

--- A2/Tile.cpp
+++ A2/Tile.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 #include "Tile.h"
-//Intilizes the tile and what type it is.
+//Intilizes the tile and what type of tile it is, as in which colour.
 Tile::Tile(char tile) :
     tile(tile)
 {
 }
 
-//Copies the tile
+//Makes a copy of the tile
 Tile::Tile(Tile& other) :
     tile(other.tile)
 {
 }
 
-//Deletes the tile
+//Deconstructor allows the tile to be deleted
 Tile::~Tile()
 {
 }
 
-//gets a tile
+//Function that gets a specific tile and returns that tile
 char Tile::getTile()
 {
     return tile;
